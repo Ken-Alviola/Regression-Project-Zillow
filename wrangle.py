c22df2ab0aedfff13e5eb758f1319dd171cbe278
Ken-Alviola <kenalviola@gmail.com>
2021-03-19 13:15:33 -0500

    initial commit

---
+++ wrangle.py
@@ -0,0 +1,108 @@
+import pandas as pd
+import numpy as np
+import sklearn.preprocessing
+from sklearn.model_selection import train_test_split
+import os
+
+from env import host, user, password
+
+def get_connection(db, user=user, host=host, password=password):
+    '''
+    This function uses my info from my env file to
+    create a connection url to access the Codeup db.
+    '''
+    return f'mysql+pymysql://{user}:{password}@{host}/{db}'
+
+def new_zillow_data():
+    '''
+    This function reads the zillow data from the Codeup db into a df,
+    write it to a csv file, and returns the df.
+    '''
+    # Create SQL query.
+    sql_query = ''' select * 
+                    from properties_2017
+                    join predictions_2017 using(parcelid)
+                    where transactiondate between "2017-05-01" and "2017-08-31"
+                    and unitcnt = 1;
+                    '''
+    
+    # Read in DataFrame from Codeup db.
+    df = pd.read_sql(sql_query, get_connection('zillow'))
+    
+    return df
+
+def get_zillow_data(cached=True):
+    '''
+    This function reads in zillow data from Codeup database and writes data to
+    a csv file if cached == False or if cached == True reads in telco df from
+    a csv file, returns df.
+    '''
+    if cached == False or os.path.isfile('zillow.csv') == False:
+        
+        # Read fresh data from db into a DataFrame.
+        df = new_zillow_data()
+        
+        # Write DataFrame to a csv file.
+        df.to_csv('zillow.csv')
+        
+    else:
+        
+        # If csv file exists or cached == True, read in data from csv.
+        df = pd.read_csv('zillow.csv', index_col=0)
+        
+    return df
+
+def clean_zillow(df):
+    '''Takes in a df of telco data and cleans the data by replacing blanks and dropping null values. 
+    The total_charges column is then converted to float
+    
+    return: df, a cleaned pandas dataframe'''
+    df.replace(r'^\s*$', np.nan, regex=True, inplace=True)
+    df = df.dropna()
+    df['total_charges'] = df.total_charges.astype(float)
+    return df
+
+def split_zillow(df, stratify_by=None):
+    """
+    Crude train, validate, test split
+    To stratify, send in a column name
+    """
+    
+    if stratify_by == None:
+        train, test = train_test_split(df, test_size=.2, random_state=123)
+        train, validate = train_test_split(df, test_size=.3, random_state=123)
+    else:
+        train_validate, test = train_test_split(df, test_size=.2, random_state=123, stratify=df[stratify_by])
+        train, validate = train_test_split(train_validate, test_size=.3, random_state=123, stratify=train_validate[stratify_by])
+    
+    return train, validate, test
+
+def wrangle_zillow(split=False):
+    '''
+    wrangle_telco will read in our student grades as a pandas dataframe,
+    clean the data
+    split the data
+    return: train, validate, test sets of pandas dataframes from telco if split = True
+    '''
+    df = clean_telco(acquire_telco())
+    if split == True:
+        return split_telco(df)
+    else:
+        return df
+
+def scale_data(train,validate,test):
+    '''Accepts train, validate, test data frames and applies min-max scaler
+    return: train, validate, test scaled pandas dataframe'''
+    
+    scaler = sklearn.preprocessing.MinMaxScaler()
+    scaler.fit(train)
+    
+    train_scaled = scaler.transform(train)
+    validate_scaled = scaler.transform(validate)
+    test_scaled = scaler.transform(test)
+    
+    train_scaled = pd.DataFrame(train_scaled, columns=train.columns)
+    validate_scaled = pd.DataFrame(validate_scaled, columns=train.columns)
+    test_scaled = pd.DataFrame(test_scaled, columns=train.columns)
+    
+    return train_scaled, validate_scaled, test_scaled
